806113aefe397f5b54c6fd1461b440d136802930
Bidepan <951150259@qq.com>
2023-04-26 20:02:37 +0800

    新增抢包

--- bd_hqcsh.py
+++ bd_hqcsh.py
@@ -3,7 +3,7 @@
 time：2023.4.24
 cron: 0 9,18 * * *
 new Env('好奇车生活签到');
-微信小程序-好汽车生活-好物兑换
+微信小程序-好奇车生活-好物兑换
 抓包域名: https://channel.cheryfs.cn/
 抓包请求头里面: accountId 的值
 环境变量名称：hqcshck = accountId 的值
@@ -57,7 +57,7 @@ class Hqcsh():
         time.sleep(0.5)
         sign_url = "https://channel.cheryfs.cn/archer/activity-api/signinact/signin"
         jf_url = 'https://channel.cheryfs.cn/archer/activity-api/common/accountPointLeft?pointId=620415610219683840'
-
+        q_url = 'https://channel.cheryfs.cn/archer/activity-api/pointsmall/exchangeCard?pointsMallCardId=' + qiang + '&exchangeCount=1&mallOrderInputVoStr=%7B%22person%22:%22%22,%22phone%22:%22%22,%22province%22:%22%22,%22city%22:%22%22,%22area%22:%22%22,%22address%22:%22%22,%22remark%22:%22%22%7D&channel=1&exchangeType=0&exchangeNeedPoints=188&exchangeNeedMoney=0&cardGoodsItemIds='
         sign_headers = {
             'User-Agent': self.ua,
             'tenantId': self.tid,
@@ -71,20 +71,56 @@ class Hqcsh():
             'activityId': '621911913692942337',
             'accountId': self.ck,
         }
-
+        q_headers = {
+            'User-Agent': self.ua,
+            'tenantId': self.tid,
+            'activityId': '621950054462152705',
+            'accountId': self.ck,
+        }
         try:
             sign_rsp = requests.get(sign_url, headers=sign_headers)
             time.sleep(0.5)
             jf_rsp = requests.get(jf_url, headers=jf_headers)
+            time.sleep(0.5)
+            q_rsp = requests.get(q_url, headers=q_headers)
+
             if sign_rsp.json()['success'] == True:
                 if sign_rsp.json()['result']['success'] == True:
-                    xx = f"[登录]：账号{a}登录成功\n[签到]：签到成功\n[积分]：{jf_rsp.json()['result']}\n\n"
-                    print(xx)
-                    self.msg += xx
+                    if q_rsp.json()['result']['success'] == True:
+                        time.sleep(0.5)
+                        qr_url = 'https://channel.cheryfs.cn/archer/activity-api/pointsmall/exchangeCardResult?resultKey=' + \
+                                 q_rsp.json()['result']['id']
+                        qr_rsp = requests.get(qr_url, headers=q_headers)
+                        if qr_rsp.json()['result']['errMsg'] == '成功':
+                            xx = f"[登录]：账号{a}登录成功\n[签到]：签到成功\n[积分]：{jf_rsp.json()['result']}\n[抢包]：{qr_rsp.json()['result']['errMsg']}，前往个人中心-我的礼包查看！\n\n"
+                            print(xx)
+                            self.msg += xx
+                        else:
+                            xx = f"[登录]：账号{a}登录成功\n[签到]：签到成功\n[积分]：{jf_rsp.json()['result']}\n[抢包]：{qr_rsp.json()['result']['errMsg']}\n\n"
+                            print(xx)
+                            self.msg += xx
+                    elif q_rsp.json()['result']['success'] == False:
+                        xx = f"[登录]：账号{a}登录成功\n[签到]：签到成功\n[积分]：{jf_rsp.json()['result']}\n[抢包]：{q_rsp.json()['result']['errMsg']}\n\n"
+                        print(xx)
+                        self.msg += xx
                 elif sign_rsp.json()['result']['success'] == False:
-                    xx = f"[登录]：账号{a}登录成功\n[签到]：{sign_rsp.json()['result']['message']}\n[积分]：{jf_rsp.json()['result']}\n\n"
-                    print(xx)
-                    self.msg += xx
+                    if q_rsp.json()['result']['success'] == True:
+                        time.sleep(0.5)
+                        qr_url = 'https://channel.cheryfs.cn/archer/activity-api/pointsmall/exchangeCardResult?resultKey=' + \
+                                 q_rsp.json()['result']['id']
+                        qr_rsp = requests.get(qr_url, headers=q_headers)
+                        if qr_rsp.json()['result']['errMsg'] == '成功':
+                            xx = f"[登录]：账号{a}登录成功\n[签到]：{sign_rsp.json()['result']['message']}\n[积分]：{jf_rsp.json()['result']}\n[抢包]：{qr_rsp.json()['result']['errMsg']}，前往个人中心-我的礼包查看！\n\n"
+                            print(xx)
+                            self.msg += xx
+                        else:
+                            xx = f"[登录]：账号{a}登录成功\n[签到]：{sign_rsp.json()['result']['message']}\n[积分]：{jf_rsp.json()['result']}\n[抢包]：{qr_rsp.json()['result']['errMsg']}\n\n"
+                            print(xx)
+                            self.msg += xx
+                    if q_rsp.json()['result']['success'] == False:
+                        xx = f"[登录]：账号{a}登录成功\n[签到]：{sign_rsp.json()['result']['message']}\n[积分]：{jf_rsp.json()['result']}\n[抢包]：{q_rsp.json()['result']['errMsg']}\n\n"
+                        print(xx)
+                        self.msg += xx
             elif sign_rsp.json()['success'] == False:
                 xx = f"[登录]：账号{a}登录失败，请稍后重试或者ck可能失效,当前ck：{self.ck}\n\n"
                 print(xx)
@@ -106,6 +142,12 @@ class Hqcsh():
 
 
 if __name__ == '__main__':
+    q1 = '647894196522340352'  # 188积分 1.08元
+    q2 = '622187839353806848'  # 288积分 1.88元
+    q3 = '622187928306601984'  # 588积分 3.88元
+    q4 = '622188100122075136'  # 888积分 5.88元
+    qiang = q1
+    print('\n默认设置自动抢188积分1.08元的包\n需要设置到脚本底部修改 qiang = xxx\nxxx为q1-q4对应的包\n')
     token = get_environ("hqcshck")
     msg = ''
     cks = token.split("&")
