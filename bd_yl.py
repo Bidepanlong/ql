8bd1e3d92c1d5502ae4b2866a61ea22c7b5abb37
Bidepan <951150259@qq.com>
2023-10-31 11:52:46 +0800

    10.31 安装依赖脚本

--- bd_yl.py
+++ bd_yl.py
@@ -1,8 +1,8 @@
 """
 
-time：2023.10.25
+time：2023.10.31
 cron: 1 1 1 1 1
-new Env('安装依赖');
+new Env('安装依赖（运行脚本前运行）');
 一键安装库里面脚本需要的依赖！
 
 """
@@ -14,10 +14,8 @@ yl = ['requests', 'bs4', 'user_agent']
 for i in yl:
     spec = importlib.util.find_spec(i)
     if spec is None:
-        # 如果找不到request库，则安装
         os.system("pip install --upgrade pip")
-        os.system(f"pip install -r yl.txt -i https://pypi.tuna.tsinghua.edu.cn/simple {i}")
+        os.system(f"pip install -i https://pypi.tuna.tsinghua.edu.cn/simple {i}")
         print(f"{i}安装完成")
     else:
-        # 如果已经安装，打印消息
         print(f"{i}已经安装")
